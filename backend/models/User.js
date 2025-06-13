54848af1d09b31ed86f27c847df3828b037cff35
bassmaelabassi <elabassibasma31@gmail.com>
2025-06-13 17:48:02 +0200

    des modification

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email:    { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: { type: String, required: true, minlength: 3 },
+  email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
+  password: { type: String, required: true, minlength: 6 },
   isOnline: { type: Boolean, default: false },
   lastSeen: { type: Date }
 });
